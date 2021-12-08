776c223aac275a57d43f724645b93207ac42c039
andrey.pylypchuk <andrey.pylypchuk@introlab-systems.com>
2021-12-08 03:06:19 +0200

    updated read and write functional

--- src/main/java/com/datapath/procurementdata/labeltranslator/service/DataProvider.java
+++ src/main/java/com/datapath/procurementdata/labeltranslator/service/DataProvider.java
@@ -2,6 +2,8 @@ package com.datapath.procurementdata.labeltranslator.service;
 
 import com.datapath.procurementdata.labeltranslator.domain.TranslateData;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -9,14 +11,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
 import java.util.*;
 
 import static java.lang.String.join;
 import static java.nio.file.Files.newInputStream;
+import static java.nio.file.Files.newOutputStream;
 import static java.nio.file.Path.of;
 import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DataProvider {
@@ -32,28 +39,38 @@ public class DataProvider {
     public Set<String> read() throws IOException {
         Set<String> result = new HashSet<>();
 
-        HSSFWorkbook workbook = new HSSFWorkbook(newInputStream(of(FILE_PATH)));
+        InputStream stream = newInputStream(of(FILE_PATH));
+        HSSFWorkbook workbook = new HSSFWorkbook(stream);
         HSSFSheet sheet = workbook.getSheet("Товары");
         int physicalNumberOfRows = sheet.getPhysicalNumberOfRows();
 
         for (int i = 1; i < physicalNumberOfRows; i++) {
+            log.info("Reading {} row from {} rows", i, physicalNumberOfRows);
             HSSFRow row = sheet.getRow(i);
             result.add(row.getCell(1).getStringCellValue());
             result.add(row.getCell(2).getStringCellValue());
         }
+        stream.close();
         return result;
     }
 
     public void write() throws IOException {
-        HSSFWorkbook workbook = new HSSFWorkbook(newInputStream(of(FILE_PATH)));
+        Path file = of(FILE_PATH);
+        InputStream stream = newInputStream(file);
+        HSSFWorkbook workbook = new HSSFWorkbook(stream);
         HSSFSheet sheet = workbook.getSheet("Товары");
         int physicalNumberOfRows = sheet.getPhysicalNumberOfRows();
 
         for (int i = 1; i < physicalNumberOfRows; i++) {
+            log.info("Writing {} row from {} rows", i, physicalNumberOfRows);
             HSSFRow row = sheet.getRow(i);
             put(row, 1, 5, 7);
             put(row, 2, 6, 8);
         }
+        stream.close();
+        OutputStream outputStream = newOutputStream(file);
+        workbook.write(outputStream);
+        outputStream.close();
     }
 
     private void put(HSSFRow row, int sourceIdx, int uaIdx, int enIdx) {
@@ -75,8 +92,10 @@ public class DataProvider {
             }
         });
 
-        row.getCell(uaIdx).setCellValue(join(", ", uaWords));
-        row.getCell(enIdx).setCellValue(join(", ", enWords));
+        HSSFCell uaCell = row.createCell(uaIdx);
+        uaCell.setCellValue(join(", ", uaWords));
+        HSSFCell enCell = row.createCell(enIdx);
+        enCell.setCellValue(join(", ", enWords));
     }
 
     private TranslateData get(String w) {
